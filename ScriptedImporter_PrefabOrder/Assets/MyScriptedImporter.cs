1a86b1d22f5bf4c741dd7f97ebed5879bbd8b54b
Seanba <sean@seanba.com>
2019-10-08 23:20:39 -0500

    Example files that display bug

---
+++ ScriptedImporter_PrefabOrder/Assets/MyScriptedImporter.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Experimental.AssetImporters;
+
+[ScriptedImporter(1, "xxx")]
+public class MyScriptedImporter : ScriptedImporter
+{
+    public override void OnImportAsset(AssetImportContext ctx)
+    {
+        // Create our main object
+        var name = Path.GetFileNameWithoutExtension(ctx.assetPath);
+        var main = new GameObject(name);
+        ctx.AddObjectToAsset(name, main);
+        ctx.SetMainObject(main);
+
+        // Instantiate a prefab to add to our main object
+        // Bug: The object heirarchy of children in our prefab is not preserved. Instead, the children are ordered alphabetically.
+        var path = "Assets/TestPrefab.prefab";
+        var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+        var instance = PrefabUtility.InstantiatePrefab(prefab, main.transform) as GameObject;
+        ctx.DependsOnSourceAsset(path);
+    }
+}
+
